e15995d09a2049f1d624adf584d4640eb6968e29
majdyaq <majd@feelit.tech>
2023-12-23 02:20:01 +0200

    themes and colors

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif"
+  },
+  // Colors for layout
+  colors: {
+    primary1: "#4CE68C",
+    background1: "#42556F",
+    button: "#4CE682",
+    background2: "#19212C",
+    text: "#C8CFD8",
+    text1: "#F2F5F7",
+    text2: "#626970",
+    text3: "#575C66",
+    footerBackground: "#00012B"
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+};
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg: "#525265",
+    bgLight: "#415352",
+    primary: "#2B943E",
+    text_primary: "#F2F3F4",
+    text_secondary: "#b1b2b3",
+    card: "#171721",
+    card_light: '#191924',
+    button: "#4CE687",
+    white: "#FFFFFF",
+    black: "#000000",
+};
+
+export const lightTheme = {
+    bg: "#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary: "#be1adb",
+    text_primary: "#111111",
+    text_secondary: "#48494a",
+    card: "#FFFFFF",
+    button: "#5c5b5b",
+};
